8e32178d4a241f69e49e16ee00490db1e3f924d1
mingeon cho <mincoln419@naver.com>
2023-12-28 10:33:01 +0900

    Container Widget

--- lib/main.dart
+++ lib/main.dart
@@ -3,41 +3,48 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     home: Scaffold(
-        appBar: AppBar(
-          actions: [
-            IconButton(icon: Icon(Icons.home), onPressed: (){
-              print('Tab!!');
-            },),
-            Icon(Icons.play_arrow),
-          ],
-          centerTitle: false,
-          title: Text('This is App bar')
-        ),
-        body: TestWidget(),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.bug_report),
-        onPressed: (){
-          print('Tab! FAB');
-        },
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text('Study to Container'),
       ),
+      body: CustomContainer(),
     ),
   ));
 }
 
-class TestWidget extends StatelessWidget {
-  const TestWidget({super.key});
+class CustomContainer extends StatelessWidget {
+  const CustomContainer({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Center(
-        child: Text(
-            'Hello, Flutter',
-            style: TextStyle(
-              fontSize: 40,
-              color: Colors.black,
-            )
-        ),
+    return Center(
+      child: Container(
+        width: 300,
+        height: 300,
+        padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
+        margin: EdgeInsets.symmetric(vertical: 12, horizontal: 10),
+        decoration: BoxDecoration(
+            color: Color(0xFFCEF6A9),
+            border: Border.all(
+                color: Colors.red, width: 5, style: BorderStyle.solid),
+            borderRadius: BorderRadius.circular(100),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withOpacity(0.3),
+                offset: Offset(6, 6),
+                blurRadius: 10,
+                spreadRadius: 10
+              ),
+              BoxShadow(
+                  color: Colors.blue.withOpacity(0.3),
+                  offset: Offset(-6, -6),
+                  blurRadius: 10,
+                  spreadRadius: 10
+              )
+            ]),
+        child: Center(
+            child: Container(
+                color: Color(0xFFF3EABC), child: Text('Hello Container'))),
       ),
     );
   }
