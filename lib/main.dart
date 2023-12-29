da43a934ec88fc697451c79b9e2c06d37cab5b05
mermer vivo <mincoln419@naver.com>
2023-12-29 17:05:19 +0900

    setting 초기화

--- lib/main.dart
+++ lib/main.dart
@@ -17,35 +17,6 @@ class CustomContainer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        width: 300,
-        height: 300,
-        padding: EdgeInsets.fromLTRB(10, 10, 10, 10),
-        margin: EdgeInsets.symmetric(vertical: 12, horizontal: 10),
-        decoration: BoxDecoration(
-            color: Color(0xFFCEF6A9),
-            border: Border.all(
-                color: Colors.red, width: 5, style: BorderStyle.solid),
-            borderRadius: BorderRadius.circular(100),
-            boxShadow: [
-              BoxShadow(
-                color: Colors.black.withOpacity(0.3),
-                offset: Offset(6, 6),
-                blurRadius: 10,
-                spreadRadius: 10
-              ),
-              BoxShadow(
-                  color: Colors.blue.withOpacity(0.3),
-                  offset: Offset(-6, -6),
-                  blurRadius: 10,
-                  spreadRadius: 10
-              )
-            ]),
-        child: Center(
-            child: Container(
-                color: Color(0xFFF3EABC), child: Text('Hello Container'))),
-      ),
-    );
+    return const Placeholder();
   }
 }
