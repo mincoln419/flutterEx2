3a717aca564273db3404b47c43f1aed2e5afc068
mermer vivo <mincoln419@naver.com>
2023-12-29 18:34:33 +0900

    refs #3 scroll view 설정

--- lib/main.dart
+++ lib/main.dart
@@ -17,6 +17,114 @@ class CustomContainer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Column(
+      children: [
+        Container(
+          color: Colors.red,
+          width: 100,
+          height: 100,
+          margin: EdgeInsets.symmetric(vertical: 10),
+        ),
+        Expanded(
+          child: Container(
+            color: Colors.red,
+            width: 100,
+            height: 100,
+            margin: EdgeInsets.symmetric(vertical: 10),
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  Container(
+                    color: Colors.blue,
+                    height: 50,
+                    width: 50,
+                    margin: EdgeInsets.symmetric(vertical: 10),
+                  ),
+                  Container(
+                    color: Colors.blue,
+                    height: 50,
+                    width: 50,
+                    margin: EdgeInsets.symmetric(vertical: 10),
+                  ),
+                  Container(
+                    color: Colors.blue,
+                    height: 50,
+                    width: 50,
+                    margin: EdgeInsets.symmetric(vertical: 10),
+                  ),
+                  Container(
+                    color: Colors.blue,
+                    height: 50,
+                    width: 50,
+                    margin: EdgeInsets.symmetric(vertical: 10),
+                  ),
+                  Container(
+                    color: Colors.blue,
+                    height: 50,
+                    width: 50,
+                    margin: EdgeInsets.symmetric(vertical: 10),
+                  ),
+                  Container(
+                    color: Colors.blue,
+                    height: 50,
+                    width: 50,
+                    margin: EdgeInsets.symmetric(vertical: 10),
+                  ),
+                  Container(
+                    color: Colors.blue,
+                    height: 50,
+                    width: 50,
+                    margin: EdgeInsets.symmetric(vertical: 10),
+                  ),
+                  Container(
+                    color: Colors.blue,
+                    height: 50,
+                    width: 50,
+                    margin: EdgeInsets.symmetric(vertical: 10),
+                  ),
+                  Container(
+                    color: Colors.blue,
+                    height: 50,
+                    width: 50,
+                    margin: EdgeInsets.symmetric(vertical: 10),
+                  ),
+                  Container(
+                    color: Colors.blue,
+                    height: 50,
+                    width: 50,
+                    margin: EdgeInsets.symmetric(vertical: 10),
+                  ),
+                  Container(
+                    color: Colors.blue,
+                    height: 50,
+                    width: 50,
+                    margin: EdgeInsets.symmetric(vertical: 10),
+                  ),
+                  Container(
+                    color: Colors.blue,
+                    height: 50,
+                    width: 50,
+                    margin: EdgeInsets.symmetric(vertical: 10),
+                  ),
+
+                ],
+              ),
+            ),
+          ),
+        ),
+        Container(
+          color: Colors.red,
+          width: 100,
+          height: 100,
+          margin: EdgeInsets.symmetric(vertical: 10),
+        ),
+        Container(
+          color: Colors.red,
+          width: 100,
+          height: 100,
+          margin: EdgeInsets.symmetric(vertical: 10),
+        ),
+      ],
+    );
   }
 }
