e160ebbb58831f4582c5d07b322d54bc50c6f802
mermer vivo <mincoln419@naver.com>
2023-12-29 18:53:16 +0900

    refs #5_stack widget

--- lib/main.dart
+++ lib/main.dart
@@ -17,6 +17,59 @@ class CustomContainer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Stack(
+      children: [
+        Container(
+          height: 500,
+          width: 500,
+          color: Colors.black,
+        ),
+        Container(
+          height: 400,
+          width: 400,
+          color: Colors.yellow,
+        ),
+        Align(
+          alignment: Alignment(0.1, -0.1),
+          child: Container(
+            height: 300,
+            width: 300,
+            color: Colors.white,
+          ),
+        ),
+        Positioned(
+          bottom: 0,
+          right: 10,
+          child: Container(
+            height: 200,
+            width: 200,
+            color: Colors.green,
+          ),
+        ),
+        Align(
+          alignment: Alignment.center,
+          child: Container(
+            height: 100,
+            width: 100,
+            decoration: BoxDecoration(
+              color: Colors.pink,
+              borderRadius: BorderRadius.circular(150)
+            ),
+          ),
+        ),
+        Align(
+          alignment: Alignment.center,
+          child: Container(
+            height: 90,
+            width: 90,
+            decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(140)
+            ),
+          ),
+        ),
+        Container(color: Colors.red, child: Text("hello world")),
+      ],
+    );
   }
 }
