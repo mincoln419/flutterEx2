7730232c3e55af45256cebac05228741e7af8873
mermer vivo <mincoln419@naver.com>
2023-12-30 18:22:05 +0900

    refs #6 stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -17,6 +17,60 @@ class CustomContainer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Column(children: [
+      ExampleStateLess(),
+      ExampleStateful(),
+    ]);
+  }
+}
+
+class ExampleStateLess extends StatelessWidget {
+  const ExampleStateLess({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      flex: 1,
+      child: Container(
+        color: Colors.red,
+      ),
+    );
+  }
+}
+
+class ExampleStateful extends StatefulWidget {
+  const ExampleStateful({super.key});
+
+  @override
+  State<ExampleStateful> createState() => _ExampleStatefulState();
+}
+
+class _ExampleStatefulState extends State<ExampleStateful> {
+  int index = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      flex: 1,
+      child: GestureDetector(
+        onTap: () {
+          setState(() {
+            if (index == 5) {
+              index = 0;
+              return;
+            }
+            index++;
+            print('$index');
+          });
+        },
+        child: Container(
+            color: Colors.blue.withOpacity(index/5),
+            child: Center(
+                child: Text(
+              '$index',
+              style: TextStyle(fontSize: 40),
+            ))),
+      ),
+    );
   }
 }
