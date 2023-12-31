700398692271ce4e2cd29eff53fb046d75826efd
mermer vivo <mincoln419@naver.com>
2024-01-01 07:55:22 +0900

    refs #15 theme component 분리

--- lib/main.dart
+++ lib/main.dart
@@ -3,21 +3,25 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
 void main() {
-  runApp(
-    MaterialApp.router(
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.indigo)
-      ),
-      routerConfig: GoRouter(
-        routes: [
-          GoRoute(path: '/', name: 'home', builder: (context, _) => HomeWidget()),
-          GoRoute(path: '/new1', name: 'page1', builder: (context, _) => NewPage()),
-          GoRoute(path: '/new2', name: 'page2', builder: (context, _) => NewPage2()),
-        ]
-      ),
-    )
-  );
+  runApp(MaterialApp.router(
+    theme: customTheme,
+    routerConfig: GoRouter(routes: [
+      GoRoute(path: '/', name: 'home', builder: (context, _) => HomeWidget()),
+      GoRoute(path: '/new1', name: 'page1', builder: (context, _) => NewPage()),
+      GoRoute(
+          path: '/new2', name: 'page2', builder: (context, _) => NewPage2()),
+    ]),
+  ));
 }
+
+final customTheme = ThemeData(
+    useMaterial3: true,
+    textTheme: const TextTheme(
+        bodyMedium: TextStyle(
+      fontWeight: FontWeight.bold,
+      fontSize: 40,
+    )),
+    colorScheme: ColorScheme.fromSeed(seedColor: Colors.indigo));
 
 class HomeWidget extends StatelessWidget {
   const HomeWidget({super.key});
