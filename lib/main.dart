bd137d694c64f6ca94f0aea64dc21d2da6a50ac7
mermer vivo <mincoln419@naver.com>
2023-12-31 15:43:47 +0900

    refs #9 callback event

--- lib/main.dart
+++ lib/main.dart
@@ -17,6 +17,74 @@ class CustomContainer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return TestWidget();
+  }
+}
+
+class TestWidget extends StatefulWidget {
+  const TestWidget({super.key});
+
+  @override
+  State<TestWidget> createState() => _TestWidgetState();
+}
+
+class _TestWidgetState extends State<TestWidget> {
+  int value = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Center(
+          child: Text(
+            'Count: $value',
+            style: const TextStyle(fontSize: 40),
+          ),
+        ),
+        TestButton(callback2: addSpecificCounter)
+      ],
+    );
+  }
+
+  void addCounter(){
+    setState(() {
+      value++;
+    });
+  }
+
+  void addSpecificCounter(int specific){
+    setState(() {
+      value += specific;
+    });
+  }
+}
+
+class TestButton extends StatelessWidget {
+  const TestButton({required this.callback2, super.key});
+
+//  final VoidCallback callback;
+
+  final Function(int) callback2;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        margin: EdgeInsets.symmetric(vertical: 8),
+        width: double.infinity,
+        child: InkWell(
+          onTap: () => callback2(4),
+          onDoubleTap: () => callback2(40),
+          onLongPress: () => callback2(400),
+          child: Center(
+              child: Container(
+            padding: EdgeInsets.all(20),
+            decoration: BoxDecoration(border: Border.all()),
+            child: const Text(
+              'Up Count',
+              style: TextStyle(fontSize: 24),
+            ),
+          )),
+        ));
   }
 }
